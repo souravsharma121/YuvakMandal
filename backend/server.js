d3acf02fb68c0696cc4a945920f72fd9e5a6cd3b
Sourav Sharma <souravsharma181818@gmail.com>
2025-04-20 14:30:18 +0530

    added deployment

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/users');
 const notificationRoutes = require('./routes/notifications');
 const contributionRoutes = require('./routes/contributions');
-
+const path = require('path')
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -26,5 +26,22 @@ app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/notifications', notificationRoutes);
 app.use('/api/contributions', contributionRoutes);
+
+
+// ---------------deployment-----------------//
+// ---------------deployment-----------------//
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+  // Serve static files from the React app
+  const staticPath = path.join(__dirname1, '../frontend/YuvakMandal/dist');
+  app.use(express.static(staticPath));
+
+  // Handle React routing, return all requests to React app
+  app.get('/', (req, res) => {
+    const indexPath = path.join(staticPath, 'index.html');
+    console.log('Index Path:', indexPath); 
+    res.sendFile(indexPath);
+  });
+}
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
